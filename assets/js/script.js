fad1116fd6226f90609a17793541796bbd9e1440
Darryl Subal <dsubal@att.net>
2023-03-22 18:47:09 -0400

    update CSS, HTML, add quiz questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,49 @@
 // variables
 
 // questions array
+var questions = [
+  {
+    question: "Commonly used data types DO NOT include:",
+    choices: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+    answer: "3. alerts",
+  },
+  {
+    question: "The condition in an if/else statement is enclosed within _____.",
+    choices: [
+      "1. quotes",
+      "2. curly brackets",
+      "3. parentheses",
+      "4. square brackets",
+    ],
+    answer: "3. parentheses",
+  },
+  {
+    question: "Arrays in JavaScript can be used to store _____.",
+    choices: [
+      "1. numbers and strings",
+      "2. other arrays",
+      "3. booleans",
+      "4. all of the above",
+    ],
+    answer: "2. other arrays",
+  },
+  {
+    question:
+      "String values must be enclosed within _____ when being assigned to variables.",
+    choices: ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"],
+    answer: "3. quotes",
+  },
+  {
+    question:
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: [
+      "1. Javascript",
+      "2. terminal / bash",
+      "3. for looops",
+      "4. console.log",
+    ],
+    answer: "4. console.log",
+  },
+];
+
+// Variables
